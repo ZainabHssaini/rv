1e6bc769d5f4601b3720a4d74db273838897a001
OthamaneELBOUR <elbour.othmane@gmail.com>
2025-05-06 00:05:22 +0100

    Firebase with credentials for Zainab

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,13 +9,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCA8vPvfXTiKa3Cih2xGyLmxG6YWjCUe5U",
-  authDomain: "reviva-4f3d6.firebaseapp.com",
-  projectId: "reviva-4f3d6",
-  storageBucket: "reviva-4f3d6.firebasestorage.app",
-  messagingSenderId: "350430675243",
-  appId: "1:350430675243:web:1095eb5e0358bfa617c56e",
-  measurementId: "G-8NK8PRYQ59"
+  apiKey: "AIzaSyASi3TNbjxrLIQ_t14pRC-phaG6DTCRrBA",
+  authDomain: "reviva-2cb77.firebaseapp.com",
+  projectId: "reviva-2cb77",
+  storageBucket: "reviva-2cb77.firebasestorage.app",
+  messagingSenderId: "892854798565",
+  appId: "1:892854798565:web:34d0fe6f508a277ca6ac0c"
 };
 
 // Initialize Firebase
